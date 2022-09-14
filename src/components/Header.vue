757592bd0947d694e0c14568e85a5a7427dfdc57
mezez <ekemammezez@gmail.com>
2022-09-14 21:03:44 +0100

    template conditionals

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,9 +2,9 @@
   <header>
     <h1>{{ title }}</h1>
     <Button
-      @toggle-add-task="$emit('toggle-add-task')"
-      text="Add Task"
-      colour="green"
+      @btn-click="$emit('toggle-add-task')"
+      :text="showAddTask ? 'Close' : 'Add Task'"
+      :colour="showAddTask ? 'red' : 'green'"
     />
   </header>
 </template>
@@ -15,6 +15,7 @@ export default {
   name: "Header",
   //   props: ["title"],
   props: {
+    showAddTask: Boolean,
     // title: {
     //   type: String,
     //   default: "Hello World",
@@ -24,7 +25,7 @@ export default {
   components: {
     Button,
   },
-  emits: ["toggle-add-task"],
+  emits: ["btn-click"],
   // setup() {
 
   // },
