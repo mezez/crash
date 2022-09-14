4819f85ea04c09649df177eea13b45844fbdc5b0
mezez <ekemammezez@gmail.com>
2022-09-14 19:44:12 +0100

    initial commit

---
+++ src/components/Button.vue
@@ -0,0 +1,21 @@
+<template>
+  <button @click="onClick()" :style="{ background: colour }" class="btn">
+    {{ text }}
+  </button>
+</template>
+
+<script>
+export default {
+  name: "Button",
+  props: {
+    text: String,
+    colour: String,
+  },
+  methods: {
+    onClick() {
+      console.log("click");
+    },
+  },
+  setup() {},
+};
+</script>
---
+++ src/components/Header.vue
@@ -0,0 +1,36 @@
+<template>
+  <header>
+    <h1>{{ title }}</h1>
+    <Button text="Add Task" colour="green" />
+  </header>
+</template>
+
+<script>
+import Button from "./Button.vue";
+export default {
+  name: "Header",
+  //   props: ["title"],
+  props: {
+    // title: {
+    //   type: String,
+    //   default: "Hello World",
+    // },
+    title: String,
+  },
+  components: {
+    Button,
+  },
+  // setup() {
+
+  // },
+};
+</script>
+
+<style scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+}
+</style>
